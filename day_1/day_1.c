02ccc3d79cca4e2795ea5eb583c13211e23a0e20
ArthurPV <arthurpontiervalois@gmail.com>
2024-12-04 21:40:34 -0500

    Add part 2 for day 1.

--- day_1/day_1.c
+++ day_1/day_1.c
@@ -9,16 +9,25 @@
 /// @param ns int* (&)
 /// @param ms int* (&)
 static void
-split_on_new_line_and_sort(char *start, int *ns, int *ms, void (*sort)(int n, int *ns, size_t len));
+split_on_new_line_and_process(char *start, int *ns, int *ms, void (*process)(int n, int *ns, size_t len));
 
-static 
-void sort(int n, int *ns, size_t len);
+static void
+sort(int n, int *ns, size_t len);
+
+/// @param ns int* (&)
+static void
+add(int n, int *ns, size_t len);
 
 /// @param file_content char* (&)
-static void part_1(char *file_content);
+static void
+part_1(char *file_content);
+
+/// @param file_content char* (&)
+static void
+part_2(char *file_content);
 
 void
-split_on_new_line_and_sort(char *start, int *ns, int *ms, void (*sort)(int n, int *ns, size_t len)) {
+split_on_new_line_and_process(char *start, int *ns, int *ms, void (*process)(int n, int *ns, size_t len)) {
 	assert(start && "start need cannot be NULL");
 
 	char *current = start;
@@ -38,8 +47,8 @@ split_on_new_line_and_sort(char *start, int *ns, int *ms, void (*sort)(int n, in
 			int n = atoi(n_s);
 			int m = atoi(m_s);
 
-			sort(n, ns, line_count);
-			sort(m, ms, line_count);
+			process(n, ns, line_count);
+			process(m, ms, line_count);
 
 			start_line_pos = (current - start) + 1;
 
@@ -50,7 +59,8 @@ split_on_new_line_and_sort(char *start, int *ns, int *ms, void (*sort)(int n, in
 	}
 }
 
-void sort(int n, int *ns, size_t len)
+void
+sort(int n, int *ns, size_t len)
 {
 	assert(len < MAX_LINE && "the length need to be less than MAX_LINE");
 
@@ -85,11 +95,20 @@ void sort(int n, int *ns, size_t len)
 	}
 }
 
-void part_1(char *file_content) {
+void
+add(int n, int *ns, size_t len)
+{
+	assert(len < MAX_LINE && "the length need to be less than MAX_LINE");
+
+	ns[len] = n;
+}
+
+void
+part_1(char *file_content) {
 	int ns[MAX_LINE] = {0};
 	int ms[MAX_LINE] = {0};
 	
-	split_on_new_line_and_sort(file_content, ns, ms, &sort);
+	split_on_new_line_and_process(file_content, ns, ms, &sort);
 
 	int total_distance = 0;
 
@@ -100,10 +119,35 @@ void part_1(char *file_content) {
 	printf("Part 1 = %d\n", total_distance);
 }
 
+void
+part_2(char *file_content) {
+	int ns[MAX_LINE] = {0};
+	int ms[MAX_LINE] = {0};
+
+	split_on_new_line_and_process(file_content, ns, ms, &add);
+
+	int total_similarity = 0;
+
+	for (size_t i = 0; i < MAX_LINE; ++i) {
+		int count = 0;
+
+		for (size_t j = 0; j < MAX_LINE; ++j) {
+			if (ns[i] == ms[j]) {
+				++count;
+			}
+		}
+
+		total_similarity += ns[i] * count;
+	}
+
+	printf("Part 2 = %d\n", total_similarity);
+}
+
 int main() {
 	char *file_content = read_file__Base("./input.txt");
 
 	part_1(file_content);
+	part_2(file_content);
 
 	free(file_content);
 }
